fe8ecb6f10369764945855fcd03c97aff2bce5fd
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-07-23 18:00:15 +0530

    create nav bar

---
+++ src/componets/Nav.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {getTopNav} from '../data/navbar'
+
+const Navbar = () => {
+    const [navItems, setNavItems] = useState([]);
+    const [collapser, setCollapser] = useState('nav_menu');
+    const [togglerIcon, setTogglerIcon] = useState('toggler_icon')
+    useEffect(() => {
+        setNavItems(getTopNav())
+    },[]);
+    const ontoggler = () => {
+        collapser === 'nav_menu'
+        ? setCollapser('nav_collapse nav_menu')
+        : setCollapser('nav_menu');
+        togglerIcon === 'toggler_icon'
+        ? setTogglerIcon('toggler toggler_icon')
+        : setTogglerIcon('toggler_icon');
+    }
+    return (
+        <div className="nav_wrapper">
+            <div className="container">
+                <nav className="nav">
+                    <a href="#" className="nav_brand">
+                        AAAAA
+                    </a>
+                    <ul className={collapser}>
+                        {navItems.map(item => (
+                            <li key={item.id} className="nav_item">
+                            <a href={item.href} className="nav_link">{item.label}</a>
+                        </li>
+                        ))}
+                        <li className="nav_item">
+                            <a href="" className="nav_link"></a>
+                        </li>
+                    </ul>
+                    <div className={togglerIcon} onClick={ontoggler}>
+                        <div className="line_1"></div>
+                        <div className="line_2"></div>
+                        <div className="line_3"></div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/data/navbar.js
@@ -0,0 +1,10 @@
+const topNav = [
+    {id:1, label:'Home', href:'#'},
+    {id:1, label:'Skills', href:'#'},
+    {id:1, label:'Projects', href:'#'},
+    {id:1, label:'Contact', href:'#'},
+];
+
+export const getTopNav = () => {
+    return topNav
+}
