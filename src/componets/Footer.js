7e59e3f5258b14fe73a806bd528a1078d076d493
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-07-29 16:08:15 +0530

    create footer

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BsLinkedin,BsTwitter,BsWhatsapp,BsFacebook,BsInstagram,BsGithub} from 'react-icons/bs';
 
 function Footer() {
     return ( 
         <div>
-            
+            <hr />
+            <div className='footer_text'>
+                <h3>© 2022 Laviru Dilshan All Rights Reserved</h3>
+                <div className="footer_icons">
+                    <a href="http://"><BsLinkedin /></a>
+                    <a href="http://"><BsTwitter /></a>
+                    <a href="http://"> <BsGithub /></a>
+                    <a href="http://"><BsWhatsapp /></a>
+                    <a href="http://"><BsFacebook /></a>
+                    <a href="http://"><BsInstagram /></a>
+                </div>
+            </div>     
         </div>
      );
 }
