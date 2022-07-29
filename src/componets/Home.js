2e34b5573696bbc1a38079ac66896ae46dfb5e43
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-07-29 10:04:34 +0530

    style Home

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -7,6 +7,8 @@ function Home() {
             <h1 className='text'>Hellow I'm</h1>
             <h1>Laviru Dilshan</h1>
             <h5>Fullstack Developer</h5>
+            <a href=""><button className='btn'>Download CV</button></a>
+            <a href=""><button className='btn-primary'>Contact Me</button></a>
         </div>
     );
 }
