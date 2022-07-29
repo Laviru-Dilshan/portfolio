18df74467d885b5e4c1bb926e6863bee78800c0d
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-07-29 09:49:15 +0530

    create develop home componet

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -4,7 +4,9 @@ import '../css/home.css'
 function Home() {
     return (  
         <div className='homebg'>
-            <h1 className='text'>Hellow World;</h1>
+            <h1 className='text'>Hellow I'm</h1>
+            <h1>Laviru Dilshan</h1>
+            <h5>Fullstack Developer</h5>
         </div>
     );
 }
