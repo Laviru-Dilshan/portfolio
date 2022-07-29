654c28261df3fc6f91ed4cf131b1d3ef42830d23
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-07-29 09:04:16 +0530

    remove imgs

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import img from '../img/moon.jpg'
 import '../css/home.css'
 
 function Home() {
     return (  
         <div className='homebg'>
-            <img src={img} className='moon' alt="moon img"/>
             <h1 className='text'>Hellow World;</h1>
         </div>
     );
