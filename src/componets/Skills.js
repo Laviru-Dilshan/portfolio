08ddcc9c976db11fabeae9ff5f93454bea028dec
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-07-31 11:49:34 +0530

    add skills

--- src/componets/Skills.js
+++ src/componets/Skills.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {FaReact} from 'react-icons/fa';
 
 function Skills() {
-    return ( 
+     return ( 
         <div>
+            <h1 className='title'>Few Skills i Have</h1>
+            <ul className="skillsList">
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                <li className='li-item'><FaReact size={200}/><br />React Js</li>
+                
             
+            </ul>
         </div>
      );
 }
