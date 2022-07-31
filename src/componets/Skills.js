05a24585969306ade65da45ad25a506f4bd717d4
Black Owl <93317127+Laviru-Dilshan@users.noreply.github.com>
2022-07-31 12:45:41 +0530

    change icon size

--- src/componets/Skills.js
+++ src/componets/Skills.js
@@ -7,16 +7,16 @@ function Skills() {
         <div>
             <h1 className='title'>Few Skills i Have</h1>
             <ul className="skillsList">
-                <li className='li-item'><div className='anim-icon'><FaReact size={200}/></div><br />React Js</li>
-                <li className='li-item'><div className='anim-icon'><FaNode size={200}/></div><br />Node Js</li>
-                <li className='li-item'><div className='anim-icon'><FaJsSquare size={200}/></div><br />Java Script</li>
-                <li className='li-item'><div className='anim-icon'><FaPhp size={200}/></div><br />PHP</li>
-                <li className='li-item'><div className='anim-icon'><SiMysql size={200}/></div><br />My Sql</li>
-                <li className='li-item'><div className='anim-icon'><SiMongodb size={200}/></div><br />Mongo DB</li>
-                <li className='li-item'><div className='anim-icon'><FaHtml5 size={200}/></div><br />Html</li>
-                <li className='li-item'><div className='anim-icon'><FaCss3Alt size={200}/></div><br />Css</li>
-                <li className='li-item'><div className='anim-icon'><FaBootstrap size={200}/></div><br />Bootstrap</li>
-                <li className='li-item'><div className='anim-icon'><FaPython size={200}/></div><br />Python</li>
+                <li className='li-item'><div className='anim-icon'><FaReact size={175}/></div><br />React Js</li>
+                <li className='li-item'><div className='anim-icon'><FaNode size={175}/></div><br />Node Js</li>
+                <li className='li-item'><div className='anim-icon'><FaJsSquare size={175}/></div><br />Java Script</li>
+                <li className='li-item'><div className='anim-icon'><FaPhp size={175}/></div><br />PHP</li>
+                <li className='li-item'><div className='anim-icon'><SiMysql size={175}/></div><br />My Sql</li>
+                <li className='li-item'><div className='anim-icon'><SiMongodb size={175}/></div><br />Mongo DB</li>
+                <li className='li-item'><div className='anim-icon'><FaHtml5 size={175}/></div><br />Html</li>
+                <li className='li-item'><div className='anim-icon'><FaCss3Alt size={175}/></div><br />Css</li>
+                <li className='li-item'><div className='anim-icon'><FaBootstrap size={175}/></div><br />Bootstrap</li>
+                <li className='li-item'><div className='anim-icon'><FaPython size={175}/></div><br />Python</li>
 
             
             </ul>
