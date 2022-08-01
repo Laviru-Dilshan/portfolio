db44cb65b5bced536866455efdba102df1b1ffa0
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-08-01 17:59:44 +0530

    fix icon display issue

--- src/componets/Skills.js
+++ src/componets/Skills.js
@@ -4,22 +4,18 @@ import {SiMysql,SiMongodb} from 'react-icons/si';
 
 function Skills() {
      return ( 
-        <div>
+        <div className='skillsList'>
             <h1 className='title'>Few Skills i Have</h1>
-            <ul className="skillsList">
-                <li className='li-item'><div className='anim-icon'><FaReact size={200}/></div><br />React Js</li>
-                <li className='li-item'><div className='anim-icon'><FaNode size={200}/></div><br />Node Js</li>
-                <li className='li-item'><div className='anim-icon'><FaJsSquare size={200}/></div><br />Java Script</li>
-                <li className='li-item'><div className='anim-icon'><FaPhp size={200}/></div><br />PHP</li>
-                <li className='li-item'><div className='anim-icon'><SiMysql size={200}/></div><br />My Sql</li>
-                <li className='li-item'><div className='anim-icon'><SiMongodb size={200}/></div><br />Mongo DB</li>
-                <li className='li-item'><div className='anim-icon'><FaHtml5 size={200}/></div><br />Html</li>
-                <li className='li-item'><div className='anim-icon'><FaCss3Alt size={200}/></div><br />Css</li>
-                <li className='li-item'><div className='anim-icon'><FaBootstrap size={200}/></div><br />Bootstrap</li>
-                <li className='li-item'><div className='anim-icon'><FaPython size={200}/></div><br />Python</li>
-
-            
-            </ul>
+                <FaReact size={200}/>
+                <FaNode size={200}/>
+                <FaJsSquare size={200}/>
+                <FaPhp size={200}/>
+                <SiMysql size={200}/>
+                <SiMongodb size={200}/>
+                <FaHtml5 size={200}/>
+                <FaCss3Alt size={200}/>
+                <FaBootstrap size={200}/>
+                <FaPython size={200}/>
         </div>
      );
 }
--- src/data/navbar.js
+++ src/data/navbar.js
@@ -3,7 +3,7 @@ const topNav = [
     {id:1, label:'Skills', href:'#'},
     {id:1, label:'Projects', href:'#'},
     {id:1, label:'Contact', href:'#'},
-];
+]
 
 export const getTopNav = () => {
     return topNav
