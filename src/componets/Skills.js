eeaae495bbd733469a091f4e2a7c54504f91c065
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-08-02 10:57:45 +0530

    add skills icons and style it

--- src/componets/Skills.js
+++ src/componets/Skills.js
@@ -6,18 +6,18 @@ function Skills() {
      return ( 
         <div>
             <h1 className='title'>Few Skills i Have</h1>
-            <div className="skillsList">
-                <FaReact size={175}/>
-                <FaNode size={175}/>
-                <FaJsSquare size={175}/>
-                <FaPhp size={175}/>
-                <SiMysql size={175}/>
-                <SiMongodb size={175}/>
-                <FaHtml5 size={175}/>
-                <FaCss3Alt size={175}/>
-                <FaBootstrap size={175}/>
-                <FaPython size={175}/>
-            </div>
+                <ul className='skills'>
+                    <li><FaReact size={175}/><br />React Js</li>
+                    <li><FaNode size={175}/><br />Node Js</li>
+                    <li><FaJsSquare size={175}/><br /> Java Script</li>
+                    <li><FaPhp size={175}/><br /> PHP </li>
+                    <li><SiMysql size={175}/><br /> My Sql</li>
+                    <li><SiMongodb size={175}/><br /> Mongo DB </li>
+                    <li><FaHtml5 size={175}/><br /> HTMl 5</li>
+                    <li><FaCss3Alt size={175}/><br /> CSS</li>
+                    <li><FaBootstrap size={175}/><br /> Bootstrap</li>
+                    <li><FaPython size={175}/><br /> Python </li>
+                </ul>
         </div>
      );
 }
