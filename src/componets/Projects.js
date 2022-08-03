e6dce5b1799aa96e43a96a73606827144887dbb7
Laviru-Dilshan <dilshanrathnayaka31@gmail.com>
2022-08-03 17:56:08 +0530

    fix project box error

--- src/componets/Projects.js
+++ src/componets/Projects.js
@@ -8,29 +8,31 @@ function Projects() {
     return ( 
         <div>
             <h1 className='title' id='projects'>My Some Projects</h1>
-            <div className='project-box'>
-                <h2>Owl Eye</h2>
-                <img src={owl_eye} alt="Owl-EyeTool" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
-                <a href="https://github.com/Laviru-Dilshan/Owl-Eye">visit it </a>
-            </div>
-            <div className='project-box'>
-                <h2>Uptrendy.lk</h2>
-                <img src={uptrendy} alt="uptrendy-image" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
-                <a href="https://uptrendy.lk/">visit it</a>
-            </div>
-            <div className='project-box'>
-                <h2>Unique Name Generator</h2>
-                <img src={name} alt="unique-name-generator" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
-                <a href="https://unique-name-generator.web.app/">visit it</a>
-            </div>
-            <div className='project-box'>
-                <h2>Simple Calculator</h2>
-                <img src={calc} alt="Simple-Calc" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
-                <a href="https://calc.laviru.ml/">visit it </a>
+            <div className='box'>
+                <div className='project-box'>
+                    <h2>Owl Eye</h2>
+                    <img src={owl_eye} alt="Owl-EyeTool" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
+                    <a href="https://github.com/Laviru-Dilshan/Owl-Eye">visit it </a>
+                </div>
+                <div className='project-box'>
+                    <h2>Uptrendy.lk</h2>
+                    <img src={uptrendy} alt="uptrendy-image" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
+                    <a href="https://uptrendy.lk/">visit it</a>
+                </div>
+                <div className='project-box'>
+                    <h2>Unique Name Generator</h2>
+                    <img src={name} alt="unique-name-generator" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
+                    <a href="https://unique-name-generator.web.app/">visit it</a>
+                </div>
+                <div className='project-box'>
+                    <h2>Simple Calculator</h2>
+                    <img src={calc} alt="Simple-Calc" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusamus in aliquam fugiat iure facere, nisi, sint animi illo, iusto ut eum consectetur quos sapiente. Fugit id possimus minima numquam.</p>
+                    <a href="https://calc.laviru.ml/">visit it </a>
+                </div>
             </div>
         </div>
      );
